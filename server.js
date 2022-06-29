15a66723c8535c11083fb81db90f6cbfc2599ada
John Muth <johnmuthuk@gmail.com>
2022-06-29 11:35:30 -0700

    show args

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end(`Hello World! I was started with args: ${JSON.stringify(process.argv.slice(2))}`);
 
 });
 
